e774175640a54cdc650bb9a96f0bf81505b02dc9
Nathan Calef <nate@nathancalef.com>
2023-10-31 12:20:59 -0400

    more

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-// async function fetchJSON() {
-//   let response = await fetch('./localities.json');
-//   let user = await response.json();
-//   return user;
-// }
-
-// async function fetchAsync () {
-//   // await response of fetch call
-//   const response = await fetch('./localities.json');
-//   // only proceed once promise is resolved
-//   const data = await response.json();
-//   // only proceed once second promise is resolved
-//   return data;
-// }
 
 async function initMap() {
 
-// for (const a in locales) {
-//   console.log(a);
-// }
-//   let response = await fetch('./localities.json');
-//  let locales = await response.json();
-  // console.log(locales)
   // Request needed libraries.
   const { Map, InfoWindow } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
@@ -29,13 +9,9 @@ async function initMap() {
     const map = new Map(document.getElementById("map"), {
       center: { lat: 42.2625932, lng:-71.8022934 }, //Worcester MA
       zoom: 9,
-      // In the cloud console, configure this Map ID with a style that enables the
-      // "Administrative Area Level 1" feature layer.
       mapId: "155147bec9cead92",  //7ba16be0c9375fa7
     });
-    // const featureLayer = map.getFeatureLayer(
-    //   google.maps.FeatureType.ADMINISTRATIVE_AREA_LEVEL_1,
-    // );
+
     const featureLayer = map.getFeatureLayer(
       google.maps.FeatureType.LOCALITY
     );
@@ -51,29 +27,6 @@ async function initMap() {
       content: "",
       disableAutoPan: true,
     });
-
-    // // Create an array of alphabetical characters used to label the markers.
-    // const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    // // Add some markers to the map.
-    // const markers = locations.map((position, i) => {
-    //   const label = labels[i % labels.length];
-    //   const pinGlyph = new google.maps.marker.PinElement({
-    //     glyph: label,
-    //     glyphColor: "white",
-    //   });
-    //   const marker = new google.maps.marker.AdvancedMarkerElement({
-    //     position,
-    //     content: pinGlyph.element,
-    //   });
-  
-    //   // markers can only be keyboard focusable when they have click listeners
-    //   // open info window when marker is clicked
-    //   marker.addListener("gmp-click", () => {
-    //     infoWindow.setContent(position.lat + ", " + position.lng);
-    //     infoWindow.open(map, marker);
-    //   });
-    //   return marker;
-    // });
 
       switch (obj.Status) {
         case "No Class 1":
@@ -225,31 +178,5 @@ let i = 0;
 
 
   }
-  
-  // const locations = [
-  //   { lat: -31.56391, lng: 147.154312 },
-  //   { lat: -33.718234, lng: 150.363181 },
-  //   { lat: -33.727111, lng: 150.371124 },
-  //   { lat: -33.848588, lng: 151.209834 },
-  //   { lat: -33.851702, lng: 151.216968 },
-  //   { lat: -34.671264, lng: 150.863657 },
-  //   { lat: -35.304724, lng: 148.662905 },
-  //   { lat: -36.817685, lng: 175.699196 },
-  //   { lat: -36.828611, lng: 175.790222 },
-  //   { lat: -37.75, lng: 145.116667 },
-  //   { lat: -37.759859, lng: 145.128708 },
-  //   { lat: -37.765015, lng: 145.133858 }, 
-  //   { lat: -37.770104, lng: 145.143299 },
-  //   { lat: -37.7737, lng: 145.145187 },
-  //   { lat: -37.774785, lng: 145.137978 },
-  //   { lat: -37.819616, lng: 144.968119 },
-  //   { lat: -38.330766, lng: 144.695692 },
-  //   { lat: -39.927193, lng: 175.053218 },
-  //   { lat: -41.330162, lng: 174.865694 },
-  //   { lat: -42.734358, lng: 147.439506 },
-  //   { lat: -42.734358, lng: 147.501315 },
-  //   { lat: -42.735258, lng: 147.438 },
-  //   { lat: -43.999792, lng: 170.463352 },
-  // ];
   
   initMap();
